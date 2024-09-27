2a3b8ec52f5d219266bf0e8432137fbe2e502426
umidjon <saidovumidjon552@gmail.com>
2024-09-27 13:36:47 +0500

    first commit

---
+++ newFile.1.js
@@ -0,0 +1,20 @@
+const { bot } = require(".");
+
+bot.on("message", async (msg) => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
+
+  if (text === "/start") {
+    return bot.sendMessage(
+      chatId,
+      "assalomu aleykum xurmatli ${msg.from.first_name} video yasovchi botga xush kelibsz"
+    );
+  }
+
+  if (text === "/info") {
+    return bot.sendMessage(
+      chatId,
+      "video yasovchi bot haqida qisqa malumot, bu botga siz o'z ismingizni yozsangiz bu bot sizni ismingizga video tashlab beradi agar tashlab bermasa adminga murodjat qilishingizni so'rab qolamiz. admin esa bot biosida"
+    );
+  }
+});
